b15b3eacf05887ce8b4f0ffd7ce3b74f9622e7e0
Mateo <3951@holbertonschool.com>
2022-02-11 12:50:32 -0800

    2 working properly betty passed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,7 +3,10 @@
 #include <stdlib.h>
 
 /**
- *
+ *selection_sort - a function that applies the selection
+ *sort to a given array and size
+ *@array: given array
+ *@size: given size for the array
  */
 
 void selection_sort(int *array, size_t size)
@@ -14,7 +17,7 @@ void selection_sort(int *array, size_t size)
 	for (j = 0; j < size - 1; j++)
 	{
 		flag = 0;
-		for (j = i; i < size - 1; i++)
+		for (i = j; i < size - 1; i++)
 		{
 			if (flag == 0)
 			{
@@ -36,9 +39,9 @@ void selection_sort(int *array, size_t size)
 				}
 				if (minvalue > array[i + 1])
 				{
-					flag = 1, minvalue = array[i + 1]
+					flag = 1, minvalue = array[i + 1];
 					tmp = i + 1;
-				}				
+				}
 			}
 		}
 		array[tmp] = array[j];
