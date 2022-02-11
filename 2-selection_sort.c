30293da30c7150cac32ae46d0accad0189d0c60b
Mateo <3951@holbertonschool.com>
2022-02-11 12:27:36 -0800

    created file for second ex

---
+++ 2-selection_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int minvalue, tmp;
+	size_t i, j;
+
+	for (j = 0; j < size - 1; 	
+}
