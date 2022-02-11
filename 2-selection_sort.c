c4f0df15ac108603c33c7612780540e0d5fd0713
Mateo Victorica <3951@holbertonschool.com>
2022-02-11 17:40:57 -0300

    2 but has more than 40 lines

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,8 +8,41 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int minvalue, tmp;
+	int minvalue = 0, tmp = 0, flag;
 	size_t i, j;
 
-	for (j = 0; j < size - 1; 	
+	for (j = 0; j < size - 1; j++)
+	{
+		flag = 0;
+		for (j = i; i < size - 1; i++)
+		{
+			if (flag == 0)
+			{
+				if (array[i] < array[i + 1])
+				{
+					flag = 1, minvalue = array[i];
+					tmp = i;
+				}
+				if (array[i] > array[i + 1])
+				{
+					flag = 1, minvalue = array[i + 1];
+					tmp = i + 1;
+				}
+			}
+			if (flag == 1)
+			{
+				if (minvalue < array[i + 1])
+				{
+				}
+				if (minvalue > array[i + 1])
+				{
+					flag = 1, minvalue = array[i + 1]
+					tmp = i + 1;
+				}				
+			}
+		}
+		array[tmp] = array[j];
+		array[j] = minvalue;
+		print_array(array, size);
+	}
 }
