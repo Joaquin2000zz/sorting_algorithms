a720c51515e9fed3112faa9c753e6a1aac720552
Mateo <3951@holbertonschool.com>
2022-02-13 13:15:04 -0800

    wrong

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,41 +11,9 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int minvalue = 0, tmp = 0, flag;
+	int minvalue = 0, tmp = 0;
 	size_t i, j;
 
 	for (j = 0; j < size - 1; j++)
 	{
 		flag = 0;
-		for (i = j; i < size - 1; i++)
-		{
-			if (flag == 0)
-			{
-				if (array[i] < array[i + 1])
-				{
-					flag = 1, minvalue = array[i];
-					tmp = i;
-				}
-				if (array[i] > array[i + 1])
-				{
-					flag = 1, minvalue = array[i + 1];
-					tmp = i + 1;
-				}
-			}
-			if (flag == 1)
-			{
-				if (minvalue < array[i + 1])
-				{
-				}
-				if (minvalue > array[i + 1])
-				{
-					flag = 1, minvalue = array[i + 1];
-					tmp = i + 1;
-				}
-			}
-		}
-		array[tmp] = array[j];
-		array[j] = minvalue;
-		print_array(array, size);
-	}
-}
