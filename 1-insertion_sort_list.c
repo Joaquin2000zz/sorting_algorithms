c5681121ac6d8fd98833b19ca84af5b12e312eee
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-02-13 06:42:37 -0800

    fixed betty code style making a new function which swaps the nodes outside insertion function

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,51 @@
 #include "sort.h"
-#include <stdio.h>
+
+/**
+ * sorting - sort with insertion method
+ * @list: pointer to the head pointer
+ * @aux2: position which sorting gonna sort
+ * Return: void function
+ */
+void sorting(listint_t **list, listint_t *aux2)
+{
+	listint_t *swap = NULL;
+
+	while (aux2->prev)
+	{
+		if (aux2->n < aux2->prev->n)
+		{
+			/*swaping nodes:*/
+			swap = aux2->prev;
+			/*1) swaping aux2->prev conections*/
+			if (swap->prev)
+			{
+				aux2->prev = swap->prev;
+				swap->prev->next = aux2;
+			}
+			else
+			{
+				aux2->prev = NULL, *list = aux2;
+			}
+			/*2) swaping swap->next conections*/
+			if (aux2->next)
+			{
+				swap->next = aux2->next;
+				aux2->next->prev = swap;
+			}
+			else
+				swap->next = NULL;
+			/*3) swaping aux2->next and swap->prev conections*/
+			aux2->next = swap;
+			swap->prev = aux2;
+			print_list(*list);
+		}
+		if (aux2->prev)
+		{
+			if (aux2->n > aux2->prev->n)
+			aux2 = aux2->prev;
+		}
+	}
+}
 
 /**
  * insertion_sort_list - sorts using the Insertion sort algorithm
@@ -10,9 +56,7 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *aux = NULL, *aux2 = NULL;
-	listint_t *swap = NULL;
-	
-	/*int flag;*/
+
 	if (!list)
 		return;
 	aux = *list;
@@ -21,43 +65,7 @@ void insertion_sort_list(listint_t **list)
 		if (aux->n > aux->next->n)
 		{
 			aux2 = aux->next;
-			while (aux2->prev)
-			{
-				if (aux2->n < aux2->prev->n)
-				{
-					/*swaping nodes:*/
-					swap = aux2->prev;
-					/*1) swaping aux2->prev conections*/
-					if (swap->prev)
-					{
-						aux2->prev = swap->prev;
-						swap->prev->next = aux2;
-					}
-					else
-					{
-						aux2->prev = NULL, *list = aux2;
-					}
-					/*2) swaping swap->next conections*/
-					if (aux2->next)
-					{
-						swap->next = aux2->next;
-						aux2->next->prev = swap;
-					}
-					else
-						swap->next = NULL;
-					/*3) swaping aux2->next and swap->prev conections*/
-					aux2->next = swap;
-					swap->prev = aux2;
-					print_list(*list);
-			
-
-				}
-				if (aux2->prev)
-				{
-					if (aux2->n > aux2->prev->n)
-					aux2 = aux2->prev;
-				}
-			}
+			sorting(list, aux2);
 		}
 		if (!aux->next)
 			break;
